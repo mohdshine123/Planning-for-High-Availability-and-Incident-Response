f9e0c9581c2977ab4849c9154c8f254d20733220
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-17 16:34:14 +0530

    Update rds.tf

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -75,7 +75,7 @@ resource "aws_rds_cluster_instance" "udacity_instance-s" {
   count                = 2
   identifier           = "udacity-db-instance-${count.index}-s"
   cluster_identifier   = aws_rds_cluster.udacity_cluster.id
-  engine                 = "aurora-mysql"
+  #engine                 = "aurora-mysql"
   instance_class       = "db.t3.medium"
   #allocated_storage      = 20
   availability_zone      =data.aws_availability_zones.available.names[count.index]
