ad57e729835de4ebcb64a48b3c4197ed216cc647
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-17 18:08:44 +0530

    Update rds.tf

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -41,9 +41,9 @@ output "db_cluster_arn" {
   value = aws_rds_cluster.udacity_cluster.arn
 }
 
-output "db_instance_arn" {
-  value = aws_rds_cluster_instance.udacity_instance[0].arn
-}
+#output "db_instance_arn" {
+  #value = aws_rds_cluster_instance.udacity_instance[0].arn
+#}
 
 resource "aws_rds_cluster_instance" "udacity_instance" {
   count                = 2
